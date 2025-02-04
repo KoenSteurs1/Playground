24b23bd5d6ed5b0e998982b6e12b8cf6b719a6f0
KoenSteurs1 <koen.steurs@telenet.be>
2025-02-04 15:43:51 +0000

    Initial commit

---
+++ components/Placeholder.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Placeholder.css';
+
+const Placeholder = () => {
+  return (
+    <div className="placeholder-container">
+      <h1 className="placeholder-title">Welkom bij meerwaardebelastingberekenen.be </h1>
+      <p className="placeholder-text">
+      De recente invoering van een meerwaardebelasting op aandelen door de nieuwe Belgische regering heeft vragen opgeroepen over hoe beleggers hun portefeuilles het beste kunnen beheren. Van zodra de details van deze belasting bekend zijn zal deze website u de mogelijkheid bieden om vooruit te plannen en strategisch te reageren.
+      <br></br><br></br>Op ons platform kunt u eenvoudig uw beleggingsposities uploaden. Ons geavanceerd algorite analyseert uw portefeuille en stelt u verschillende strategieën voor om de impact van de meerwaardebelasting te minimaliseren.
+      <br></br><br></br>Laat ons samen trachten de impact van deze belasting op uw portefeuille te beperken.
+      </p>
+    </div>
+  );
+};
+
+export default Placeholder;
